e812e019a9066f7cbd03cb1a70dd2b8efdf63206
wwwxsv19 <wwwxsv19@gmail.com>
2023-09-28 22:18:30 +0900

    ~0982

--- 9월/0926_순위.py
+++ 9월/0926_순위.py
@@ -1,4 +1,10 @@
 n = int(input())
 num = list(map(int, input().split()))
 
-arr = sorted(num, reverse=True)
+sorted_num = sorted(num, reverse=True)
+
+print(sorted_num)
+
+for i in range(n):
+    sc = sorted_num.index(num[i]) + 1
+    print(num[i], sc)
